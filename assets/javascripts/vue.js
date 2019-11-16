ea99a66aed711b490a2449eb387442f04d1c3bf1
kiahi39 <nm1552jp@gmail.com>
2019-11-17 02:30:36 +0900

    remove test.

--- assets/javascripts/vue.js
+++ assets/javascripts/vue.js
@@ -1,23 +1,9 @@
 
 
 
-var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'こんにちは びゅう!'
-    }
-  })
-
 var menu = new Vue({
     el: '#menu',
     data: {
         activenum: 0
     }
-})
-
-var app1 =  new Vue ({
-  el: '#app1',
-  data: {
-      active01: false
-  }
 })
