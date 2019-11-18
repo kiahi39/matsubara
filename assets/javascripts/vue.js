1dc27e4a36cf869b4b7aeda97a38bf16c6aaed5a
kiahi39 <nm1552jp@gmail.com>
2019-11-18 17:03:22 +0900

    vue method for menu animation.

--- assets/javascripts/vue.js
+++ assets/javascripts/vue.js
@@ -5,5 +5,17 @@ var menu = new Vue({
     el: '#menu',
     data: {
         activenum: 0
+    },
+    methods: {
+        menuclick: function(click_menu_num) {
+            this.activenum = (this.activenum==click_menu_num) ? 0 : click_menu_num;
+            /** 
+            if(activenum == click_menu_num){
+                activenum = 0;
+            } else {
+                activenum = click_menu_num;
+            }
+            */
+        }
     }
 })
